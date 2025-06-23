4980ca7f03482bd61f571e7892191d2d9ab3673a
mafe <mvalenzuela@unal.edu.co>
2025-06-22 23:35:47 -0500

    Resultado lab9

--- homework/pregunta_14.sql
+++ homework/pregunta_14.sql
@@ -1,6 +1,6 @@
--- 
+--
 --  Sea el siguiente conjunto de tablas en una base de datos:
--- 
+--
 --    tbl0
 --    ------------------------
 --    K0  CHAR(1)
@@ -8,7 +8,7 @@
 --    c02 INT
 --    c03 CHAR(4)
 --    c04 FLOAT
---        
+--
 --    tbl1
 --    ------------------------
 --    K0  CHAR(1)
@@ -29,7 +29,7 @@
 --    c25 CHAR(5)
 --
 --  Escriba una consulta que retorne por cada letra de la
---  columna K0 de la tabla tbl1, el valor promedio de la 
+--  columna K0 de la tabla tbl1, el valor promedio de la
 --  columna c21 en la tabla tbl2, para aquellos registros
 --  con c13 mayor a 400.
 --
@@ -42,5 +42,11 @@
 --  4  E  555.323077
 --
 --  >>> Escriba su codigo a partir de este punto <<<
---  
-
+--
+SELECT t1.K0, AVG(t2.c21) AS "avg(c21)"
+FROM tbl1 t1
+    JOIN tbl2 t2 ON t1.K1 = t2.K1
+WHERE
+    t1.c13 > 400
+GROUP BY
+    t1.K0
